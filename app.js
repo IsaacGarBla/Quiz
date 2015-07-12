ba004368f40f0374f0a74a9db8651cac0d2dacb1
isaacgarbla <isaacgarbla@gmail.com>
2015-07-12 18:41:07 +0100

    Añadido layout

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// Importamos el modulo express-partials
+var partials = require('express-partials');
+
 var routes = require('./routes/index');
 
 var app = express();
@@ -12,6 +15,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+// Instalamos el MW que da soporte a las vistas parciales.
+app.use(partials());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
