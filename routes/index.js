fd2513fe219b40556409ea9a4ec1410d8fc29f87
isaacgarbla <isaacgarbla@gmail.com>
2015-07-12 23:47:07 +0100

    Rama créditos

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,11 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz'});
 });
 
+/* GET Créditos- */
+router.get('/author', function(req, res) {
+  res.render('author');
+});
+
 // Añadimos las dos acciones a controlar.
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
