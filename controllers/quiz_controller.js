1ec69239257012e83f47876949f370a07509800f
isaacgarbla <isaacgarbla@gmail.com>
2015-07-23 22:23:18 +0100

    Módulo 7. P2P Obligatorio

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -20,13 +20,20 @@ exports.load = function (req, res, next, quizId) {
 		
 
 //GET /quizes
-exports.index = function (req, res) {
-	models.Quiz.findAll().then(function(quizes) {
+exports.index= function (req, res) {
+	// Componemos la cadena a buscar sustituyendo los espacios en blanco por el caracter comodin.
+	var strLike;
+	
+	req.query.search = req.query.search || "";	// Se utiliza para el caso de que search sea 'undefined'
+	strLike = "%" + (req.query.search.replace(/( )+/g, "%") || "")  + "%";
+	console.log(strLike);
+	models.Quiz.findAll({where: ["pregunta like ?", strLike]}).then(function(quizes) {
 		// Devolvemos la lista de todas las preguntas para 
 		// renderizarla con la vista index.
 		res.render('quizes/index', {quizes: quizes});
 	  }).catch (function (error) { next(error);});
 };
+
 
 //GET quizes/:Id
 exports.show = function (req,res) {
