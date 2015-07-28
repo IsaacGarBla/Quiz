bd65ac46481f02af6196c137420e525958b40215
isaacgarbla <isaacgarbla@gmail.com>
2015-07-28 22:39:55 +0100

    Modulo 8. Editar preguntas

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 // Importamos el modulo express-partials
 var partials = require('express-partials');
@@ -24,6 +25,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
+app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
